0c18be72d65f02fca9503073562d3de545307c68
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-09-17 00:44:20 +0300

    Api Çağrıları ile Veri Senkranizasyonu gerçekleştirildi.

--- Person.API/Program.cs
+++ Person.API/Program.cs
@@ -1,3 +1,10 @@
+using Amazon.Runtime;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Person.API.Models.Entities;
+using Person.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,16 +15,64 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddSingleton<MongoDBService>();
+builder.Services.AddHttpClient("Employee.API", httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://localhost:7288");
+});
+
+
+#region MongoDB ye Seed Data Ekleme
+
+using IServiceScope scope = builder.Services.BuildServiceProvider().CreateScope();
+MongoDBService mongoDBService = scope.ServiceProvider.GetService<MongoDBService>();
+
+var collection = mongoDBService.GetCollection<Person.API.Models.Entities.Person>(); //Person tablosundaki tüm verileri collection a attýk.
+if (!collection.FindSync(s => true).Any())
+{
+    await collection.InsertOneAsync(new() { Name = "Ahmet" });
+    await collection.InsertOneAsync(new() { Name = "Nebahat" });
+    await collection.InsertOneAsync(new() { Name = "Ýbrahim" });
+    await collection.InsertOneAsync(new() { Name = "Samet" });
+    await collection.InsertOneAsync(new() { Name = "Betül" });
+    await collection.InsertOneAsync(new() { Name = "Musa" });
+
+}
+
+#endregion
+
+
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/{id}/{newName}", async (
+    [FromRoute] string id,
+    [FromRoute] string newName,
+    MongoDBService mongoDBService,
+    IHttpClientFactory httpClientFactory) =>
+{
+    var httpClient = httpClientFactory.CreateClient("Employee.API");
+
+    var persons = mongoDBService.GetCollection<Person.API.Models.Entities.Person>();
+
+    Person.API.Models.Entities.Person person = await (await persons.FindAsync(p => p.Id == ObjectId.Parse(id))).FirstOrDefaultAsync();
+    person.Name = newName;
+    await persons.FindOneAndReplaceAsync(p => p.Id == ObjectId.Parse(id), person);
+
+    var httpResponseMessage = await httpClient.GetAsync($"update/{person.Id}/{person.Name}");
+    if (httpResponseMessage.IsSuccessStatusCode)
+    {
+        var content = await httpResponseMessage.Content.ReadAsStringAsync();
+        await Console.Out.WriteLineAsync(content);
+    }
+});
 
 app.UseHttpsRedirection();
 
---
+++ Person.API/Services/MongoDBService.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace Person.API.Services
+{
+    public class MongoDBService      // Bu MongoDBService' in IOC ile irtibatlı olması için bunu gidip Program.cs de builder.Services.AddSingleton<MongoDBService>(); şeklinde IOC Container a    ekliyoruz.
+    {
+        readonly IMongoDatabase _mongoDatabase;
+
+        public MongoDBService(IConfiguration configuration)
+        {
+            MongoClient client = new(configuration.GetConnectionString("MongoDB")); //MongoClient sınıfı bizim MongoDB nin sunucusuna bağlanmamızı sağlıyo zaten MongoDB  sunucusunun ConnectionStringini alıyor. client sunucuya bağlanıyor ve bize veritabanını getiriyor.
+            _mongoDatabase = client.GetDatabase("PersonDB"); //ServicePersonDB adındaki Veritabanımızı _mongoDatabase referansına verdik.
+        }
+        //MongoDB de bir DATABASE(ServicePersonDB) den  veri okumak için  IMongoCollection türünden bir func oluşturduk.GetCollection fonksiyonu bize  ServicePersonDB(_mongoDatabase) den GetCollection fonksiyonu ile T ye karşılık gelen collectionu getirecek.Yani hangi Entity veriliyorsa o entity e karşılık gelen collection getirilecek.
+        public IMongoCollection<T> GetCollection<T>() => _mongoDatabase.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+    }
+}
