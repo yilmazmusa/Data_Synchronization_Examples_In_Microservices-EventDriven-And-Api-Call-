3a2ef6df6a18cac30ae3777b2691550dc250e1cd
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-09-16 20:22:05 +0300

    Add project files.

---
+++ Employee.API/Models/Entities/Employee.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Employee.API.Models.Entities
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement(Order = 0)]
+        public ObjectId Id { get; set; }
+
+
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement(Order =1)] 
+        public string PersonId { get; set; }
+
+
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement(Order =2)]
+        public string Name { get; set; }
+
+
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement(Order =3)]
+        public string Department { get; set; }
+
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        [BsonElement(Order =4)]
+        public decimal Salary { get; set; }
+
+
+    }
+
+    
+}
---
+++ Person.API/Models/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Person.API.Models.Entities
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement(Order = 1 )]
+        public ObjectId Id { get; set; }
+
+
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement(Order = 1)]
+        public string Name { get; set; } // Burdaki Name değeri değiştiriğinde biz Service Employee.API deki Name değerinide değiştirmek zorundayız.Veri tutarlılığı için bu önemli buna Veri Senkronizasyonu denir.
+    }
+}
